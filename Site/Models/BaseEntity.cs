031b26cf43ac56ac11c0e264a60f2fc145490f33
amircsharp7 <amir.1derful@gmail.com>
2022-05-30 22:37:15 +0430

    Update menu tbl

--- Site/Models/BaseEntity.cs
+++ Site/Models/BaseEntity.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Site.Models;
 public class BaseEntity
 {
     public int Id { get; set; }
+    [Required]
     public string CreateUserId { get; set; }
+    [Required]
     public string UpdateUserId { get; set; }
     public DateTime CreateDate { get; set; }
     public DateTime UpdateDate { get; set; }
