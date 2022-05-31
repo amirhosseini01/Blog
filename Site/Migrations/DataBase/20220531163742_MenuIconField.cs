d3b3b7d6eec7d7903b6a84ee0813170f5e8552e2
amircsharp7 <amir.1derful@gmail.com>
2022-05-31 21:09:14 +0430

    Menu → add icon class name filed

---
+++ Site/Migrations/DataBase/20220531163742_MenuIconField.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Site.Migrations.DataBase
+{
+    public partial class MenuIconField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Menus",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Menus",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IconClassName",
+                table: "Menus",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IconClassName",
+                table: "Menus");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Menus",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Menus",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- Site/Models/Menu.cs
+++ Site/Models/Menu.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Site.Models;
 public class Menu : BaseEntity
 {
+    [Required]
     public string Title { get; set; }
+    public string IconClassName { get; set; }
     public int? PId { get; set; }
+    [Required]
     public string Url { get; set; }
     public int? OrderView { get; set; }
     public bool IsHidden { get; set; }
