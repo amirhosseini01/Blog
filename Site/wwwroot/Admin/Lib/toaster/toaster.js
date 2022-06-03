86ae7e8d66f924b079b3ccf2dc0f817099c03947
amircsharp7 <amir.1derful@gmail.com>
2022-06-03 23:03:38 +0430

    js toaster custom lib

---
+++ Site/wwwroot/Admin/Lib/toaster/toaster.js
@@ -0,0 +1,15 @@
+function Toast(succeeded, message) {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    x.innerText = message;
+    if(succeeded){
+        x.className +=" bg-success"
+    }
+    else{
+        x.className +=" bg-danger"
+    }
+    
+    setTimeout(function(){ 
+        x.className = x.className.replace("show", "");
+     }, 3000);
+  }
