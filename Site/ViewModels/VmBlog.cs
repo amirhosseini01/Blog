994b681bac7bff93c34943e7ca7c0670ad3d4224
amircsharp7 <amir.1derful@gmail.com>
2022-06-03 15:39:54 +0430

    Create Detail View Model

--- Site/ViewModels/VmBlog.cs
+++ Site/ViewModels/VmBlog.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace Site.ViewModels;
 
 public class VmBlogList
@@ -11,4 +14,49 @@ public class VmBlogList
     public string PersianCreateDate { get; set; }
     public DateTime UpdateDate { get; set; }
     public string PersianUpdateDate { get; set; }
+}
+
+public class VmBlogDetail
+{
+    [DisplayName("کد")]
+    public int? Id { get; set; }
+
+    [DisplayName("دسته بندی")]
+    [Required(ErrorMessage = "{0} را انتخاب کنید.")]
+    [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "{0} را انتخاب کنید.")]
+    public int CategoryId { get; set; }
+
+    [DisplayName("عنوان")]
+    [Required(ErrorMessage = "{0} را وارد کنید.")]
+    [StringLength(maximumLength: 60, MinimumLength = 2, ErrorMessage = "{0} باید بین {2} تا {1} کاراکتر باشد.")]
+    public string Title { get; set; }
+
+    [DisplayName("توضیحات متا")]
+    [Required(ErrorMessage = "{0} را وارد کنید.")]
+    [StringLength(maximumLength: 160, MinimumLength = 2, ErrorMessage = "{0} باید بین {2} تا {1} کاراکتر باشد.")]
+    public string MetaDescription { get; set; }
+
+    [DisplayName("توضیحات")]
+    [Required(ErrorMessage = "{0} را وارد کنید.")]
+    public string Description { get; set; }
+
+    [DisplayName("عدم انتشار")]
+    public bool IsHidden { get; set; }
+
+    [DisplayName("تصویر")]
+    public string ImgUrl { get; set; }
+
+    [DisplayName("ALT تصویر")]
+    [Required(ErrorMessage = "{0} را وارد کنید.")]
+    [StringLength(maximumLength: 60, MinimumLength = 2, ErrorMessage = "{0} باید بین {2} تا {1} کاراکتر باشد.")]
+    public string ImgAlt { get; set; }
+
+    [DisplayName("توضیحات تصویر")]
+    [Required(ErrorMessage = "{0} را وارد کنید.")]
+    [StringLength(maximumLength: 60, MinimumLength = 2, ErrorMessage = "{0} باید بین {2} تا {1} کاراکتر باشد.")]
+    public string ImgTitle { get; set; }
+
+    [DisplayName("لینک مقاله اصلی")]
+    [StringLength(maximumLength: 450, ErrorMessage = "{0} باید حداکثر {1} کاراکتر باشد.")]
+    public string CanonicalUrl { get; set; }
 }
