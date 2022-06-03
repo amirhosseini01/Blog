e6fc79e00420af760561461fa73ff3f4588cdfbd
amircsharp7 <amir.1derful@gmail.com>
2022-06-03 16:12:05 +0430

    blog detail → wite all form group html of view model

--- Site/Areas/Admin/Pages/BlogDetail.cshtml.cs
+++ Site/Areas/Admin/Pages/BlogDetail.cshtml.cs
@@ -1,17 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Site.Repositories.Contracts;
+using Site.ViewModels;
 
-namespace Site.Areas_Admin_Pages
+namespace Site.Areas_Admin_Pages;
+public class BlogDetailModel : PageModel
 {
-    public class BlogDetailModel : PageModel
+    private readonly IBlogRep _blogRep;
+    public BlogDetailModel(IBlogRep blogRep)
     {
-        public void OnGet()
-        {
+        _blogRep = blogRep;
+    }
 
-        }
+    //properties
+    public int? Id { get; set; }
+    public VmBlogDetail VmInput { get; set; }
+
+    //methods
+    public void OnGet(int? id)
+    {
+        Id = id;
     }
 }
