b647fae1ad90224e82a50aed23d9a852ecdc9081
amircsharp7 <amir.1derful@gmail.com>
2022-06-09 19:50:41 +0430

    scaffold menu page

---
+++ Site/Areas/Admin/Pages/Menus.cshtml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Site.Areas_Admin_Pages;
+public class MenusModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
